831af86b5a96718c0faa09ad43524fbd758ac0d0
EmilianoNDC <emilianondocarmo@gmail.com>
2024-01-06 19:40:40 -0300

    Commit musica de nuevo

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react';
-import Navbar from '../../components/Navbar/Navbar'
+import React, { Fragment, useEffect } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 import Hero2 from '../../components/hero2/hero2';
 import CoupleSection2 from '../../components/CoupleSection2/CoupleSection2';
 import StorySection2 from '../../components/StorySection2/StorySection2';
@@ -8,49 +8,37 @@ import RsvpSection from '../../components/RsvpSection/RsvpSection';
 import EventSection from '../../components/EventSection/EventSection';
 import BlogSection from '../../components/BlogSection/BlogSection';
 import Footer from '../../components/footer/Footer';
-import Scrollbar from '../../components/scrollbar/scrollbar'
-import { useEffect } from 'react';
-
-
+import Scrollbar from '../../components/scrollbar/scrollbar';
 
 const HomePage = () => {
+  useEffect(() => {
+    // Obtén el elemento de audio
+    const audioElement = new Audio('/Ed Sheeran - Perfect.mp3');
 
-    return (
-        <Fragment>
-            <Navbar />
-            <Hero2 />
-            <CoupleSection2 />
-            <StorySection2 />
-            <PortfolioSection />
-            <RsvpSection/>
-            <EventSection/>
-            <BlogSection />
-            <Footer />
-            <Scrollbar />
-        </Fragment>
-    )
+    // Reproduce la canción cuando la página se carga
+    audioElement.play();
+
+    // Opcional: Puedes detener la reproducción al desmontar el componente
+    return () => {
+      audioElement.pause();
+      audioElement.currentTime = 0;
+    };
+  }, []); // El segundo argumento del useEffect es un array de dependencias, en este caso, está vacío para que se ejecute solo una vez al montar la página.
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Hero2 />
+      <CoupleSection2 />
+      <StorySection2 />
+      <PortfolioSection />
+      <RsvpSection />
+      <EventSection />
+      <BlogSection />
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
-
-const IndexPage = () => {
-    useEffect(() => {
-      // Obtén el elemento de audio
-      const audioElement = new Audio('/Ed Sheeran - Perfect.mp3');
-  
-      // Reproduce la canción cuando la página se carga
-      audioElement.play();
-  
-      // Opcional: Puedes detener la reproducción al desmontar el componente
-      return () => {
-        audioElement.pause();
-        audioElement.currentTime = 0;
-      };
-    }, []); // El segundo argumento del useEffect es un array de dependencias, en este caso, está vacío para que se ejecute solo una vez al montar la página.
-  
-    return (
-      <div>
-        {/* Contenido de tu página */}
-      </div>
-    );
-  };
 
 export default HomePage;
