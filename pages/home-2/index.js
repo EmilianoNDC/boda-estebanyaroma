7d9e47ce64e0ac672c18c08bedb4636f8a007de8
EmilianoNDC <emilianondocarmo@gmail.com>
2024-01-06 19:35:42 -0300

    Commit musica

--- pages/home-2/index.js
+++ pages/home-2/index.js
@@ -10,6 +10,7 @@ import EventSection from '../../components/EventSection/EventSection';
 import BlogSection from '../../components/BlogSection/BlogSection';
 import Footer from '../../components/footer/Footer';
 import Scrollbar from '../../components/scrollbar/scrollbar'
+import { useEffect } from 'react';
 
 
 
@@ -31,5 +32,27 @@ const HomePage2 = () => {
         </Fragment>
     )
 };
+
+const IndexPage = () => {
+    useEffect(() => {
+      // Obtén el elemento de audio
+      const audioElement = new Audio('/Ed Sheeran - Perfect.mp3');
+  
+      // Reproduce la canción cuando la página se carga
+      audioElement.play();
+  
+      // Opcional: Puedes detener la reproducción al desmontar el componente
+      return () => {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+      };
+    }, []); // El segundo argumento del useEffect es un array de dependencias, en este caso, está vacío para que se ejecute solo una vez al montar la página.
+  
+    return (
+      <div>
+        {/* Contenido de tu página */}
+      </div>
+    );
+  };
 
 export default HomePage2;
--- pages/home/index.js
+++ pages/home/index.js
@@ -9,6 +9,7 @@ import EventSection from '../../components/EventSection/EventSection';
 import BlogSection from '../../components/BlogSection/BlogSection';
 import Footer from '../../components/footer/Footer';
 import Scrollbar from '../../components/scrollbar/scrollbar'
+import { useEffect } from 'react';
 
 
 
@@ -29,5 +30,27 @@ const HomePage = () => {
         </Fragment>
     )
 };
+
+const IndexPage = () => {
+    useEffect(() => {
+      // Obtén el elemento de audio
+      const audioElement = new Audio('/Ed Sheeran - Perfect.mp3');
+  
+      // Reproduce la canción cuando la página se carga
+      audioElement.play();
+  
+      // Opcional: Puedes detener la reproducción al desmontar el componente
+      return () => {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+      };
+    }, []); // El segundo argumento del useEffect es un array de dependencias, en este caso, está vacío para que se ejecute solo una vez al montar la página.
+  
+    return (
+      <div>
+        {/* Contenido de tu página */}
+      </div>
+    );
+  };
 
 export default HomePage;
